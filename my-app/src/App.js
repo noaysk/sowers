236c0a7cfd2a6485cd14d671cf93f42ad8d2d7e8
noaysk <nyasukagawa@myseneca.ca>
2023-01-05 18:07:15 +0900

    created main

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,20 @@
 import "./App.css";
-import bubble from "./img/bubble.png";
-import bubblem from "./img/bubble-m.png";
+
+import title from "./img/Group 4.png";
+
+import hatena from "./img/hatena.png";
+// import bubblem from "./img/bubble-m.png";
+// import bubbleb from "./img/bubble-b.png";
+
+// import union2  from "./img/Union2.png";
+// import union3  from "./img/Union3.png";
+import union4 from "./img/Union4.png";
+
+import group1 from "./img/Group 1.png";
+import group2 from "./img/Group 2.png";
+import group3 from "./img/Group 3.png";
+
+import top1 from "./img/top1.png";
 
 // import MyInput from "./Components/MyInput";
 
@@ -13,30 +27,70 @@ function App() {
     //     <h1>Here is our main heading</h1>
     //   </div>
     // </div>
-    <div className="text-center h-screen ">
-      <div className="my-8">
-        <h1 className="text-5xl font-semibold  mt-48">
-          Here is our main heading
-        </h1>
-        <div className="">
-          <button class="border-4 bg-slate-900 text-white font-bold border-slate-900 hover:bg-slate-900/75 py-2 px-4 rounded m-5">
-            資料請求　＞
-          </button>
-          <button class="border-4              text-black font-bold border-slate-900 hover:bg-slate-300 py-2 px-4 rounded m-5">
-            お問い合わせ
-          </button>
-        </div>
-      </div>
-      <div className="">
-        <div className="flex justify-center">
-          <img className="w-80" src={bubble} alt="" />
-          <img className="w-80" src={bubblem} alt="" />
-          <img className="w-80 self-end" src={bubblem} alt="" />
+    <body className=" bg-title-green/25">
+      <div className=" ">
+        <div className="my-20">
+          <div className="flex ">
+            <div className="w-3/5">
+              <div className="bg-title-green h-128 top-48 flex justify-center">
+                <div className="pt-20 ">
+                  <div className=" text-font text-xl font-bold ">
+                    <p className="ml-10">
+                      <span className="border-dashed border-b-4 border-white ">
+                        「学ぶこと」ｘ「成長すること」
+                      </span>
+                    </p>
 
+                    {/* <div className="bg-white h-2 w-80"></div> */}
+                  </div>
+                  <img className="my-5" src={title} alt="" />
+
+                  <div className="my-10">
+                    <button class="border-4 bg-slate-900 text-white font-bold border-slate-900 hover:bg-slate-900/75 py-2 px-4 rounded m-5">
+                      資料請求　＞
+                    </button>
+                    <button class="border-4 bg-white text-black font-bold border-slate-900 hover:bg-slate-300 py-2 px-4 rounded m-5">
+                      お問い合わせ
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-2/5 mt-24">
+              <img className="my-0 mx-auto" src={top1} alt="" />
+            </div>
+          </div>
         </div>
-        <h2 className="text-3xl">プログラミングに興味はありますか？</h2>
+
+        <div className="mx-72 my-40">
+          <div className="flex">
+            <img className="ml-20" src={group1} alt="" />
+            <img className="ml-auto" src={group2} alt="" />
+          </div>
+          <h2 className="text-3xl my-10 text-center">
+            {/* <span className="inline-block bg-cyan-600"> プログラミングに興味はありますか？</span> */}
+            <div className="bg-title-green h-20 p-5 mx-80 rounded-2xl">
+              <span className="border-dashed border-b-8 border-white">
+             
+                プログラミングに興味はありますか？
+              </span>
+            </div>
+          </h2>
+          <div className="flex">
+            <img className="w-1/2" src={hatena} alt="" />
+            <img className="ml-48 h-64" src={union4} alt="" />
+          </div>
+        </div>
+
+        <div className="mx-48">
+          <div className="flex">
+            <img className="ml-32" src={group3} alt="" />
+          </div>
+          <h2 className="text-3xl my-10 mx-auto">目的別学習法から探す</h2>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
 
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: { colors: {
-      "dark-purple": "#081A51",
-      "light-white": "rgba(255,255,255,0.17)",
+    extend: {
+      colors: {
+        "dark-purple": "#081A51",
+        "light-white": "rgba(255,255,255,0.17)",
+        'title-green': '#6DBF6B',
+        'font': '#053657',
+        // 'transparent-green': '#6DBF6B',
+
+      },
+      height: {
+        '128': '32rem',
+      }
     },
   },
-  },
   plugins: [],
-}
+};
