9ccde6aa944296f769ea7cf3fa3f582b5f0f8cb0
noaysk <nyasukagawa@myseneca.ca>
2023-01-06 17:50:52 +0900

    organized components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,10 @@
 import "./App.css";
 
-import title from "./img/Group 4.png";
-
-import hatena from "./img/hatena.png";
-// import bubblem from "./img/bubble-m.png";
-// import bubbleb from "./img/bubble-b.png";
-
-// import union2  from "./img/Union2.png";
-// import union3  from "./img/Union3.png";
-import union4 from "./img/Union4.png";
-
-import group1 from "./img/Group 1.png";
-import group2 from "./img/Group 2.png";
 import group3 from "./img/Group 3.png";
 
-import top1 from "./img/top1.png";
-
 // import MyInput from "./Components/MyInput";
+import Title from "./Components/Title";
+import Interest from "./Components/Interest";
 
 function App() {
   // return<MyInput ></MyInput>;
@@ -28,76 +16,24 @@ function App() {
     //   </div>
     // </div>
     <body className=" bg-title-green/25">
-      <div className=" ">
-        <div className="my-20">
-          <div className="flex ">
-            <div className="w-3/5">
-              <div className="bg-title-green h-128 top-48 flex justify-center">
-                <div className="pt-20 ">
-                  {/* <div className=" text-font text-xl font-bold relative">
-                    <p className="ml-10 "> 
-                      <div className="absolute bottom-0 w-80 h-3 bg-white "></div>
-                      <span className="relative z-10">
-                        「学ぶこと」ｘ「成長すること」
-                      </span> */}
+      <Title />
+      <Interest />
 
-                  <div className=" text-font text-xl font-bold ">
-                    <p className="ml-10 relative z-10">
-                      <span className="relative after:absolute after:content-[''] after:bg-white after:h-[12px] after:bottom-0 after:left-0 after:right-0 after:z-[-1]">
-                        「学ぶこと」ｘ「成長すること」
-                      </span>
-
-                      {/* <span className="border-dashed border-b-4 border-white ">
-                        「学ぶこと」ｘ「成長すること」
-                      </span> */}
-                    </p>
-                  </div>
-                  <img className="my-5" src={title} alt="" />
-
-                  <div className="my-10">
-                    <button class="border-4 bg-slate-900 text-white font-bold border-slate-900 hover:bg-slate-900/75 py-2 px-4 rounded m-5">
-                      資料請求　＞
-                    </button>
-                    <button class="border-4 bg-white text-black font-bold border-slate-900 hover:bg-slate-300 py-2 px-4 rounded m-5">
-                      お問い合わせ
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="w-2/5 mt-24">
-              <img className="my-0 mx-auto" src={top1} alt="" />
-            </div>
-          </div>
-        </div>
-
-        <div className="mx-72 my-40">
-          <div className="flex">
-            <img className="ml-20" src={group1} alt="" />
-            <img className="ml-auto" src={group2} alt="" />
-          </div>
-          <h2 className="text-3xl my-10 text-center">
-            {/* <span className="inline-block bg-cyan-600"> プログラミングに興味はありますか？</span> */}
-            <div className="bg-title-green h-20 p-5 mx-80 rounded-2xl">
-              <span className="border-dashed border-b-8 border-white">
-                プログラミングに興味はありますか？
-              </span>
-            </div>
-          </h2>
-          <div className="flex">
-            <img className="w-1/2" src={hatena} alt="" />
-            <img className="ml-48 h-64" src={union4} alt="" />
-          </div>
-        </div>
-
-        <div className="mx-48">
-          <div className="flex">
-            <img className="ml-32" src={group3} alt="" />
-          </div>
-          <h2 className="text-3xl my-10 mx-auto">目的別学習法から探す</h2>
-        </div>
+      <div className="mx-48">
+          <img className="ml-32" src={group3} alt="" />
+        <h2 className=" text-3xl text-center tracking-wider">目的別学習法から探す</h2>
       </div>
+
+      <ul className="flex flex-row flex-wrap justify-center mx-24">
+  <li className="bg-title-green w-1/5 h-32 m-10">1</li>
+  <li className="bg-title-green w-1/5 h-32 m-10">2</li>
+  <li className="bg-title-green w-1/5 h-32 m-10">3</li>
+  <li className="bg-title-green w-1/5 h-32 m-10">4</li>
+
+</ul>
+
+
+
     </body>
   );
 }
