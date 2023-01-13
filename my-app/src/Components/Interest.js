c8b07d86f4ab81abd9c06912ee12a478b5467011
noaysk <nyasukagawa@myseneca.ca>
2023-01-13 22:02:50 +0900

    changed the size

--- my-app/src/Components/Interest.js
+++ my-app/src/Components/Interest.js
@@ -7,22 +7,21 @@ import hatena from "../img/hatena.png";
 const Intrest = () => {
 
   return (
-    <div className="mx-72 my-40">
+    <div className="mx-80 my-40">
     <div className="flex">
-      <img className="ml-20" src={bubble1} alt="" />
-      <img className="ml-auto" src={bubble2} alt="" />
+      <img className="object-contain w-96 ml-24" src={bubble1} alt="" />
+      <img className="ml-auto object-contain w-[30rem]" src={bubble2} alt="" />
     </div>
-    <h2 className="text-3xl my-10 text-center">
-      {/* <span className="inline-block bg-cyan-600"> プログラミングに興味はありますか？</span> */}
-      <div className="bg-title-green h-20 p-5 mx-80 rounded-2xl relative z-10">
+    <h2 className="text-3xl my-5 text-center">
+      <div className="bg-title-green h-20 p-5 mx-72 rounded-2xl relative z-10">
         <span className="relative after:absolute after:content-[''] after:bg-white after:h-[15px] after:bottom-0 after:left-0 after:right-0 after:z-[-1] after:rounded tracking-wider">
-          プログラミングに興味はありますか？
+           プログラミングに興味はありますか？ 
         </span>
       </div>
     </h2>
-    <div className="flex">
-      <img className="w-1/2" src={hatena} alt="" />
-      <img className="ml-48 h-64" src={bubble3} alt="" />
+    <div className="flex mr-24">
+      <img className="object-contain w-[32rem]" src={hatena} alt="" />
+      <img className="ml-auto object-contain w-[28rem]" src={bubble3} alt="" />
     </div>
   </div>
   );
