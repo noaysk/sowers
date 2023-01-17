b22d7f11f53de189da6815c081f3df0ae05bde36
noaysk <nyasukagawa@myseneca.ca>
2023-01-17 11:30:19 +0900

    added join.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,17 @@ import Title from "./Components/Title";
 import Interest from "./Components/Interest";
 import Way from "./Components/Way";
 import Triangle from "./Components/Triangle";
+import Join from "./Components/Join";
 
 
 function App() {
   return (
-    <body className=" bg-title-green/25 font-sans">
+    <body className=" bg-title-green/25 font-sans text-font-color">
       <Title />
       <Interest />
       <Way />
       <Triangle />
+      <Join />
     </body>
   );
 }
