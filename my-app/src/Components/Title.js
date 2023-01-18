99846c9afd3919579b3c23a5c0f6f8c9fc5ab3e2
noaysk <nyasukagawa@myseneca.ca>
2023-01-18 09:09:33 +0900

    fixed space

--- my-app/src/Components/Title.js
+++ my-app/src/Components/Title.js
@@ -3,34 +3,20 @@ import title from "../img/title.png";
 import top1 from "../img/top1.png";
 
 const Title = () => {
-
   return (
     <div className=" ">
-   
       <div className="flex ">
         <div className="w-3/5">
           <div className="bg-title-green h-128 top-48 flex justify-center">
             <div className="pt-20 ">
-              {/* <div className=" text-font text-xl font-bold relative">
-                <p className="ml-10 "> 
-                  <div className="absolute bottom-0 w-80 h-3 bg-white "></div>
-                  <span className="relative z-10">
-                    「学ぶこと」ｘ「成長すること」
-                  </span> */}
-
               <div className=" text-font text-xl font-bold ">
                 <p className="ml-10 relative z-10">
                   <span className="relative after:absolute after:content-[''] after:bg-white after:h-[12px] after:bottom-0 after:left-0 after:right-0 after:z-[-1] after:rounded-sm">
                     「学ぶこと」ｘ「成長すること」
                   </span>
-
-                  {/* <span className="border-dashed border-b-4 border-white ">
-                    「学ぶこと」ｘ「成長すること」
-                  </span> */}
                 </p>
               </div>
               <img className="my-5 " src={title} alt="" />
-
               <div className="my-10">
                 <button class="border-4 bg-slate-900 text-white font-bold border-slate-900 hover:bg-slate-900/75 py-2 px-4 rounded m-5">
                   資料請求　＞
@@ -42,7 +28,6 @@ const Title = () => {
             </div>
           </div>
         </div>
-
         <div className="w-2/5 ">
           <img className="my-0 mx-auto" src={top1} alt="" />
         </div>
--- my-app/src/Components/Triangle.js
+++ my-app/src/Components/Triangle.js
@@ -1,15 +1,13 @@
 import React from "react";
 import triangle from "../img/triangle.png";
 
-
 const Triangle = () => {
-
   return (
     <div className="">
-          <img className="mt-24 my-10 mx-auto" src={triangle} alt="" />
-          <img className="my-10 mx-auto" src={triangle} alt="" />
-          <img className="my-10 mx-auto" src={triangle} alt="" />
-  </div>
+      <img className="mt-24 my-10 mx-auto" src={triangle} alt="" />
+      <img className="my-10 mx-auto" src={triangle} alt="" />
+      <img className="my-10 mx-auto" src={triangle} alt="" />
+    </div>
   );
 };
 
